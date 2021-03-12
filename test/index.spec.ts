0fa1a7b9f3b511689a2debaf58c01f6e5c884af4
Gabriel Duarte <gabrielduarte@claretiano.edu.br>
2021-03-12 13:43:10 +0000

    Recompilação 12/03/2021 13:01:10

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -4,12 +4,12 @@ import Url from '../src';
 
 /* Tests */
 describe('index.ts', (): void => {
-    it('matchUrl', async (): Promise<void> => {
+    it('1. matchUrl', async (): Promise<void> => {
         expect(Url.matchUrl('invalid url')).to.be.false;
         expect(Url.matchUrl('https://test.com')).to.be.true;
     });
 
-    it('matchUri', async (): Promise<void> => {
+    it('2. matchUri', async (): Promise<void> => {
         expect(Url.matchUri('invalid uri')).to.be.false;
         expect(Url.matchUri('/test/test2')).to.be.true;
     });
